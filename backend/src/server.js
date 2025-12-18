d7f86cb2b699d7ba7bcb0f27a5847ef2efccaa27
sagarmeravi <sagarmeravi563@gmail.com>
2025-12-18 16:13:53 +0530

    login-logout functionality added

---
+++ backend/src/server.js
@@ -0,0 +1,31 @@
+import express from "express";
+import path from "path";
+
+import authRoutes from "./routes/auth.route.js";
+import messageRoutes from "./routes/message.route.js";
+import { connectDB } from "./lib/db.js";
+import { ENV } from "./lib/env.js";
+
+const app = express();
+const __dirname = path.resolve();
+
+const PORT = ENV.PORT || 3000;
+
+app.use(express.json()); // req.body
+
+app.use("/api/auth", authRoutes);
+app.use("/api/messages", messageRoutes);
+
+// make ready for deployment
+if (ENV.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
+
+  app.get("*", (_, res) => {
+    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+  });
+}
+
+app.listen(PORT, () => {
+  console.log("Server running on port: " + PORT);
+  connectDB();
+});
