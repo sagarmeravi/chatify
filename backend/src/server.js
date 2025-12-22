990d870fd7a7f0d0804a1c9cd72fd2f8969a45ca
sagarmeravi <sagarmeravi563@gmail.com>
2025-12-22 15:35:19 +0530

    signup page created

--- backend/src/server.js
+++ backend/src/server.js
@@ -5,7 +5,7 @@ import authRoutes from "./routes/auth.route.js";
 import messageRoutes from "./routes/message.route.js";
 import { connectDB } from "./lib/db.js";
 import { ENV } from "./lib/env.js";
-
+import cors from "cors";
 const app = express();
 const __dirname = path.resolve();
 
@@ -13,6 +13,7 @@ const PORT = ENV.PORT || 3000;
 
 app.use(express.json()); // req.body
 app.use(cookieParser());
+app.use(cors({ origin: ENV.CLIENT_URL, credentials: true }));
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
 
--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,29 +1,46 @@
-import React from "react";
-import { Routes, Route } from "react-router";
+import { Navigate, Route, Routes } from "react-router";
 import ChatPage from "./pages/ChatPage";
 import LoginPage from "./pages/LoginPage";
 import SignUpPage from "./pages/SignUpPage";
 import { useAuthStore } from "./store/useAuthStore";
+import { useEffect } from "react";
+import PageLoader from "./components/PageLoader";
+
+import { Toaster } from "react-hot-toast";
 
 function App() {
-  const { authUser, isLoading, login } = useAuthStore();
-  console.log("isLoading", isLoading);
-  console.log("isLoading", isLoading);
+  const { checkAuth, isCheckingAuth, authUser } = useAuthStore();
+
+  useEffect(() => {
+    checkAuth();
+  }, [checkAuth]);
+
+  if (isCheckingAuth) return <PageLoader />;
+
   return (
     <div className="min-h-screen bg-slate-900 relative flex items-center justify-center p-4 overflow-hidden">
+      {/* DECORATORS - GRID BG & GLOW SHAPES */}
       <div className="absolute inset-0 bg-[linear-gradient(to_right,#4f4f4f2e_1px,transparent_1px),linear-gradient(to_bottom,#4f4f4f2e_1px,transparent_1px)] bg-[size:14px_24px]" />
       <div className="absolute top-0 -left-4 size-96 bg-pink-500 opacity-20 blur-[100px]" />
       <div className="absolute bottom-0 -right-4 size-96 bg-cyan-500 opacity-20 blur-[100px]" />
-      <button className="z-10" onClick={login}>
-        Click me
-      </button>
+
       <Routes>
-        <Route path="/" element={<ChatPage></ChatPage>}></Route>
-        <Route path="/login" element={<LoginPage></LoginPage>}></Route>
-        <Route path="/signup" element={<SignUpPage></SignUpPage>}></Route>
+        <Route
+          path="/"
+          element={authUser ? <ChatPage /> : <Navigate to={"/login"} />}
+        />
+        <Route
+          path="/login"
+          element={!authUser ? <LoginPage /> : <Navigate to={"/"} />}
+        />
+        <Route
+          path="/signup"
+          element={!authUser ? <SignUpPage /> : <Navigate to={"/"} />}
+        />
       </Routes>
+
+      <Toaster />
     </div>
   );
 }
-
 export default App;
