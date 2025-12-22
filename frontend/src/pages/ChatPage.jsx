267e018db47bb500761fe08c4d2fc6783244fc81
sagarmeravi <sagarmeravi563@gmail.com>
2025-12-22 20:57:43 +0530

    Login page implemented

--- frontend/src/pages/ChatPage.jsx
+++ frontend/src/pages/ChatPage.jsx
@@ -1,11 +1,12 @@
-import React from "react";
+import { useAuthStore } from "../store/useAuthStore";
 
 function ChatPage() {
+  const { logout } = useAuthStore();
   return (
-    <div>
-      <h1>Chat Page </h1>
+    <div className="z-10">
+      ChatPage
+      <button onClick={logout}>logout</button>
     </div>
   );
 }
-
 export default ChatPage;
